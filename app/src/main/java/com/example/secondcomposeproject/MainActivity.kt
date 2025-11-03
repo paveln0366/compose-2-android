596b76a0fa3c91c84d1aba7ef8c0f4dcaccf4d67
Pavel Potapov <potapov@goozix.com>
2025-11-03 13:48:52 +0300

    Added SwipeToDismiss

--- app/src/main/java/com/example/secondcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/secondcomposeproject/MainActivity.kt
@@ -7,13 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.secondcomposeproject.ui.theme.InstagramProfileCard
 import com.example.secondcomposeproject.ui.theme.SecondComposeProjectTheme
@@ -38,14 +47,41 @@ private fun Test(viewModel: MainViewModel) {
                 .background(MaterialTheme.colorScheme.background)
         ) {
             val models = viewModel.models.observeAsState(listOf())
-            LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-                items(models.value) { model ->
-                    InstagramProfileCard(
-                        model = model,
-                        onFollowedButtonClickListener = {
-                            viewModel.changeFollowingStatus(it)
+            val lazyListState = rememberLazyListState()
+            LazyColumn {
+                items(models.value, key = { it.id }) { model ->
+                    val dismissState = rememberSwipeToDismissBoxState()
+
+                    if (dismissState.currentValue == SwipeToDismissBoxValue.EndToStart) {
+                        viewModel.delete(model)
+                    }
+                    SwipeToDismissBox(
+                        state = dismissState,
+                        enableDismissFromStartToEnd = false,
+                        backgroundContent = {
+                            Box(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxSize()
+                                    .background(Color.Red.copy(alpha = 0.5f)),
+                                contentAlignment = Alignment.CenterEnd
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(16.dp),
+                                    text = "Delete item",
+                                    color = Color.White,
+                                    fontSize = 24.sp
+                                )
+                            }
                         }
-                    )
+                    ) {
+                        InstagramProfileCard(
+                            model = model,
+                            onFollowedButtonClickListener = {
+                                viewModel.changeFollowingStatus(it)
+                            }
+                        )
+                    }
                 }
             }
         }
